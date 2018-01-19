7363114d0fcd22e5e02564f322fbe82b81fb4362
abhidemon <abhishek.singh@unbxd.com>
2018-01-20 00:27:33 +0530

    build: Testing

---
+++ solr/guesschema.sh
@@ -0,0 +1,3 @@
+bin/solr restart -c -p 8983   -a "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=20000"
+bin/solr zk upconfig -n guess -z localhost:9983 -d  /Users/abhi/Downloads/solrcloud/cores/guessschemacore &&
+curl -v 'http://localhost:8983/solr/admin/collections?_=1493961385150&action=CREATE&collection.configName=guess&maxShardsPerNode=1&name=coll1&numShards=1&replicationFactor=1&router.name=compositeId&routerName=compositeId&wt=json'
